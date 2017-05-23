8c0ba62ce1ebfcb0708d8eaab7871ba6701774cb
ACandeias <a.candeias@nycmlt4990c.fordfound.org>
2017-05-23 09:37:17 -0400

    IDS Files

---
+++ Windows Event Logs/reg-bonus-plus.ps1
@@ -0,0 +1,29 @@
+# THIS SCRIPT IS PROVIDED "AS IS" WITH NO WARRANTIES OR GUARANTEES OF ANY
+# KIND, INCLUDING BUT NOT LIMITED TO MERCHANTABILITY AND/OR FITNESS FOR A
+# PARTICULAR PURPOSE. ALL RISKS OF DAMAGE REMAINS WITH THE USER, EVEN IF THE
+# AUTHOR, SUPPLIER OR DISTRIBUTOR HAS BEEN ADVISED OF THE POSSIBILITY OF ANY
+# SUCH DAMAGE. IF YOUR STATE DOES NOT PERMIT THE COMPLETE LIMITATION OF
+# LIABILITY, THEN DO NOT DOWNLOAD OR USE THE SCRIPT. NO TECHNICAL SUPPORT
+# WILL BE PROVIDED.
+#
+# Ignore these keys
+$whitelist="iTunesHelper","SunJavaUpdateSched","Adobe ARM","QuickTime Task"
+# Initialize the $count HashTable (associative array)
+$count=@{} 
+# Read file, find lines with ÒREG_SZÓ
+$keynames=Get-Content \labs\reg.txt|select-string "REG_SZ"
+# Iterate through $keynames one key at a time
+foreach ( $key in $keynames ) {
+  # Remove Ò REG_SZÓ to the end of the line. 
+  # Same as this regex: s/ REG_SZ.*//g 
+  $key=$key -replace " *REG_SZ.*", ""
+  # Remove extra spaces
+  $key=$key.trim()
+  # If it’s not whitelisted
+  if (-not ($whitelist -contains $key)){
+    # Increment count for that key by 1
+    $count[$key]++ 
+  }
+}
+# print each key and its count, sorted from highest to lowest
+$count.GetEnumerator() | sort value -Descending    
---
+++ Windows Event Logs/reg-bonus.ps1
@@ -0,0 +1,24 @@
+# THIS SCRIPT IS PROVIDED "AS IS" WITH NO WARRANTIES OR GUARANTEES OF ANY
+# KIND, INCLUDING BUT NOT LIMITED TO MERCHANTABILITY AND/OR FITNESS FOR A
+# PARTICULAR PURPOSE. ALL RISKS OF DAMAGE REMAINS WITH THE USER, EVEN IF THE
+# AUTHOR, SUPPLIER OR DISTRIBUTOR HAS BEEN ADVISED OF THE POSSIBILITY OF ANY
+# SUCH DAMAGE. IF YOUR STATE DOES NOT PERMIT THE COMPLETE LIMITATION OF
+# LIABILITY, THEN DO NOT DOWNLOAD OR USE THE SCRIPT. NO TECHNICAL SUPPORT
+# WILL BE PROVIDED.
+#
+# Initialize the $count HashTable (associative array)
+$count=@{} 
+# Read file, find lines with ÒREG_SZÓ
+$keynames=Get-Content \labs\reg.txt|select-string "REG_SZ"
+# Iterate through $keynames one key at a time
+foreach ( $key in $keynames ) {
+  # Remove Ò REG_SZÓ to the end of the line. 
+  # Same as this regex: s/ REG_SZ.*//g 
+  $key=$key -replace " *REG_SZ.*", ""
+  # Remove extra spaces
+  $key=$key.trim()
+  # Increment count for this key by 1
+  $count[$key]++ 
+}
+# print each key and its count, sorted from highest to lowest
+$count.GetEnumerator() | sort value -Descending    
